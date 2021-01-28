08922848541c3cd5d750cfdeba83c9222923d3c5
CCB-ISENP16 <christopher.cabantous@isen.yncrea.fr>
2021-01-28 11:09:19 +0000

    feature : exit properly when CTRL+C is pressed

--- smartSurge/main.py
+++ smartSurge/main.py
@@ -1,4 +1,13 @@
 from outlets.outlet import Outlet
+import time
 
 heater = Outlet(topic="heater", name="Heater", pinNumber=17)
 light = Outlet(topic="light", name="Light", pinNumber=18)
+
+while True:
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        heater.Stop()
+        light.Stop()
+        break
