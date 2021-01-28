33208360a8b0d67ac3f014c577d097aec42c1b5d
CCB-ISENP16 <christopher.cabantous@isen.yncrea.fr>
2021-01-28 17:32:27 +0000

    feature : add DS18B20 in main program

--- smartSurge/main.py
+++ smartSurge/main.py
@@ -1,8 +1,10 @@
 from outlets.outlet import Outlet
+from sensors.ds18b20 import DS18B20
 import time
 
 heater = Outlet(topic="heater", name="Heater", pinNumber=17)
 light = Outlet(topic="light", name="Light", pinNumber=18)
+ds18b20 = DS18B20(topic="thermometer",name="Thermometer")
 
 while True:
     try:
@@ -10,4 +12,5 @@ while True:
     except KeyboardInterrupt:
         heater.Stop()
         light.Stop()
+        ds18b20.Stop()
         break
