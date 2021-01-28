59655f1dc2e989ff3f7b4770b24e3919cba0f244
CCB-ISENP16 <christopher.cabantous@isen.yncrea.fr>
2021-01-28 17:14:26 +0000

    feature : add thermometer feature, read value from file and sent it through MQTT

---
+++ smartSurge/sensors/ds18b20.py
@@ -0,0 +1,75 @@
+import json
+import time
+import glob
+from datetime import datetime
+from threading import Thread
+
+
+# from influxdb import InfluxDBClient
+from mqtt.client import MqttClient
+from mqtt.interfaceconnector import IMqttConnector
+
+# https://fr.pinout.xyz/pinout/wiringpi
+
+# pin 4 data DS18B
+
+
+class DS18B20(Thread):
+    def __init__(self, topic, name):
+        super().__init__()
+        self.__state = False
+        self.__name = name
+        self.__topic = topic
+        self.__temperature = 0.0
+        self.__base_dir = '/sys/bus/w1/devices/'
+        self.__device_folder = glob.glob(self.__base_dir + '28*')[0]
+        self.__device_file = self.__device_folder + '/w1_slave'
+
+        self.__mqtt = MqttClient(
+            self, "127.0.0.1", ["cmd/"+self.__topic+"/TEMP"], self.__name+"-DS18B20")
+
+        self.__running = True
+        self.start()
+
+    def Receive(self, server, topic: str, payload: bytes):
+        print("")
+
+    def Send(self, topic, msg):
+        self.__mqtt.sendMessage(topic, msg)
+
+    def Connected(self, server):
+        pass
+
+    def Acknowledge(self, server, messageId: int):
+        pass
+
+    def read_temp_raw(self):
+        f = open(self.__device_file, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def read_temp(self):
+        lines = self.read_temp_raw()
+        while lines[0].strip()[-3:] != 'YES':
+            time.sleep(0.2)
+            lines = self.read_temp_raw()
+        equals_pos = lines[1].find('t=')
+        if equals_pos != -1:
+            temp_string = lines[1][equals_pos+2:]
+            temp_c = float(temp_string) / 1000.0
+            return temp_c
+
+    def run(self):
+        while True:
+            time.sleep(5)
+            self.__temperature = self.read_temp()
+            self.Send("stat/"+self.__topic+"/RESULT",
+                      str(round(self.__temperature, 1)) + " °C")
+
+    def Stop(self):
+        self.__mqtt.Halt()
+        print("[{}] closed".format("Sensor-"+self.__topic))
+        self.__running = False
+        self.join()
+        print("Stop DS18B20")
