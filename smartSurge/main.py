0c30035f536144a7e443e0926bb99cbc6fefe835
CCB-ISENP16 <christopher.cabantous@isen.yncrea.fr>
2021-01-29 21:19:00 +0000

    feature : activate water flow sensor

--- smartSurge/main.py
+++ smartSurge/main.py
@@ -1,11 +1,12 @@
 from outlets.outlet import Outlet
 from sensors.ds18b20 import DS18B20
+from sensors.fs300a import FS300A
 import time
 
 heater = Outlet(topic="heater", name="Heater", pinNumber=17)
 light = Outlet(topic="light", name="Light", pinNumber=18)
-ds18b20 = DS18B20(topic="thermometer",name="Thermometer")
-
+ds18b20 = DS18B20(topic="thermometer", name="Thermometer")
+fs300a = FS300A(pinNumber=20, topic="FS300A", name="WaterSensorFlow")
 while True:
     try:
         time.sleep(1)
@@ -13,4 +14,5 @@ while True:
         heater.Stop()
         light.Stop()
         ds18b20.Stop()
+        fs300a.Stop()
         break
